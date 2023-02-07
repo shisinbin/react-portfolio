eca11256707da22a963a646f7b3d6eae5e880b04
ssb <sb1664@gmail.com>
2023-02-07 22:41:24 +0000

    add a basic form with some basic styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,12 @@ function Contact() {
       <h2>Need some help?</h2>
       <p>Tell me what you want, and I'll build it. Well, I can try.</p>
       <p>Email link</p>
-      <p>Form</p>
+      <form>
+        <input type='text' placeholder='NAME' />
+        <input type='email' placeholder='EMAIL' />
+        <textarea placeholder='YOUR MESSAGE'></textarea>
+        <button>Reach out</button>
+      </form>
     </section>
   );
 }
