99180eb62931ae9b34a203b3169df99e376075b7
ssb <sb1664@gmail.com>
2023-02-09 21:11:36 +0000

    add functionality for form to send a message to email, make small changes to text to reflect that I'm a freelancer available for work

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,15 @@ function About() {
       <img src={aboutImage} alt='about' />
       <div className='about-content'>
         <h4 className='trail'>About</h4>
-        <h2>Web Developer</h2>
+        <h2>Freelance Web Developer</h2>
         <p>
-          Hi there! My name is Shiv and I'm a web developer on a mission. While
-          I may not have a background in design, I've got a passion for solving
-          problems with code and making beautiful, functional websites. I know I
-          still have a lot to learn, but I'm excited to continue growing and
-          finding my place in the world of web development. So, let's see what I
-          can do!
+          I'm Shiv, a freelance web developer constantly striving to create
+          clean, functional, and aesthetically pleasing websites. My passion
+          lies in finding solutions to complex problems through code, and I'm
+          always eager to continue learning and growing in the field of web
+          development. As a freelancer, I'm available for hire and ready to
+          bring your vision to life. Let's work together to create something
+          truly exceptional.
         </p>
       </div>
     </section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,106 @@
+import { useState } from 'react';
+
 function Contact() {
+  const [success, setSuccess] = useState(false);
+
+  const [isSending, setIsSending] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+    access_key: '5173651c-9d37-405d-92ab-7063a90bc333',
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setIsSending(true);
+
+    const data = JSON.stringify(formData);
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsSending(false);
+        setSuccess(true);
+        setFormData({
+          ...formData,
+          name: '',
+          email: '',
+          message: '',
+        });
+
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <section id='contact'>
       <div className='contact-content'>
         <h4 className='trail'>Contact</h4>
         <h2>Let's Connect!</h2>
         <p>
-          Whether you need a hand with a project or just want to say hi, I'm
-          here. Shoot me a message and let's chat!
+          Whether you're interested in hiring a freelance web developer, looking
+          to collaborate on a project, or just want to say hi, I'd love to hear
+          from you. Fill out the form below and let's start a conversation.
         </p>
         <a href='mailto:sb1664@gmail.com'>
           <i className='far fa-envelope'></i> sb1664@gmail.com
         </a>
       </div>
-      <form>
-        <input type='text' placeholder='NAME' />
-        <input type='email' placeholder='EMAIL' />
-        <textarea placeholder='YOUR MESSAGE'></textarea>
-        <button>
-          Reach out &nbsp; <i className='fas fa-paper-plane'></i>
-        </button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          name='name'
+          onChange={handleChange}
+          type='text'
+          placeholder='NAME'
+          value={formData.name}
+        />
+        <input
+          name='email'
+          onChange={handleChange}
+          type='email'
+          placeholder='EMAIL'
+          value={formData.email}
+        />
+        <textarea
+          name='message'
+          onChange={handleChange}
+          placeholder='YOUR MESSAGE'
+          rows='4'
+          value={formData.message}
+        ></textarea>
+        <div className='form-container'>
+          <button>
+            Reach out &nbsp; <i className='fas fa-paper-plane'></i>
+          </button>
+        </div>
       </form>
+
+      {isSending && <p style={{ color: '#128bad' }}>Sending message...</p>}
+
+      {success && (
+        <p style={{ color: '#128bad' }}>Message successfully sent!</p>
+      )}
     </section>
   );
 }
